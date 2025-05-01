e4eb4c9fc2bc736cc76c5e61477da2ae0c06278f
GiftedR <kidsgamers@hotmail.com>
2025-05-01 08:52:27 -0700

    Added Project Reference, Added crud to AssignmentService.cs

--- AssignmentLibrary/AssignmentService.cs
+++ AssignmentLibrary/AssignmentService.cs
@@ -9,6 +9,7 @@ public class AssignmentService
 		return Assignments;
 	}
 
+	// Create Assignment
 	public void AddAssignment(Assignment assignment)
 	{
 		if (Contains(assignment))
@@ -19,9 +20,46 @@ public class AssignmentService
 		}
 	}
 
+	// Read All Assignment
 	public List<Assignment> ListIncomplete()
 	{
 		return Assignments.Where(a => a.IsCompleted == false).ToList();
+	}
+
+	// Read One Assignment
+	public Assignment? FindAssignmentByTitle(string title) 
+		=> Assignments.FirstOrDefault(assi => assi.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+
+	// Update Assignment IsComplete
+	public bool MarkAssignmentComplete(string title)
+	{
+		Assignment? completeAssignment = FindAssignmentByTitle(title);
+
+		if (completeAssignment == null) return false;
+
+		completeAssignment.MarkComplete();
+		return true;
+	}
+	public bool UpdateAssigment(string oldTitle, string newTitle, string newDescription)
+	{
+		Assignment? updateAssignment = FindAssignmentByTitle(oldTitle);
+		if (updateAssignment == null) return false;
+		if (!oldTitle.Equals(newTitle, StringComparison.OrdinalIgnoreCase)
+			&& Assignments.Any(assi => assi.Title.Equals(newTitle, StringComparison.OrdinalIgnoreCase))
+			) return false;
+		
+		updateAssignment.Update(newTitle, newDescription);
+		return true;
+	}
+	// Delete Assignment
+	public bool DeleteAssignment(string title)
+	{
+		Assignment? deleteAssignment = FindAssignmentByTitle(title);
+
+		if (deleteAssignment == null) return false;
+
+		Assignments.Remove(deleteAssignment);
+		return true;
 	}
 
 	private bool Contains(Assignment assignment)
