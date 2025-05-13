33ff18648fe09da940f4c99c433706e543ff5216
GiftedR <kidsgamers@hotmail.com>
2025-05-13 08:43:48 -0700

    Added Interfaces in AssignmentLibrary

---
+++ AssignmentLibrary/Interfaces/IAppLogger.cs
@@ -0,0 +1,6 @@
+namespace AssignmentLibrary.Interfaces;
+
+public interface IAppLogger
+{
+    void Log(string message);
+}
---
+++ AssignmentLibrary/Interfaces/IAssignmentFormatter.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace AssignmentLibrary.Interfaces;
+
+public interface IAssignmentFormatter
+{
+	public string Format(Assignment assignment);
+}
