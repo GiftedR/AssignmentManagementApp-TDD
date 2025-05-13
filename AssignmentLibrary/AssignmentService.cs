62229784bebcc15512899de8b20f7e2ed6a6df0f
GiftedR <kidsgamers@hotmail.com>
2025-05-13 09:25:34 -0700

    Added Logging for actions made with AssignmentService

--- AssignmentLibrary/AssignmentService.cs
+++ AssignmentLibrary/AssignmentService.cs
@@ -6,6 +6,14 @@ public class AssignmentService : IAssignmentService
 {
 	private List<Assignment> Assignments = new List<Assignment>();
 
+	private IAssignmentFormatter _formatter; 
+	private IAppLogger _logger;	
+	public AssignmentService(IAssignmentFormatter formatter, IAppLogger logger)
+	{
+		_formatter = formatter;
+		_logger = logger;
+	}
+
 	public List<Assignment> ListAll()
 	{
 		return Assignments;
@@ -16,8 +24,10 @@ public class AssignmentService : IAssignmentService
 	{
 		if (Contains(assignment))
 		{
+			_logger.Log("Attempted to add duplicate assignment. Throwing Exception...");
 			throw new InvalidDataException("Cannot have duplicate Assignments");
 		} else {
+			_logger.Log($"New Assignment Added: {_formatter.Format(assignment)}");
 			Assignments.Add(assignment);
 			return true;
 		}
@@ -38,7 +48,12 @@ public class AssignmentService : IAssignmentService
 	{
 		Assignment? completeAssignment = FindAssignmentByTitle(title);
 
-		if (completeAssignment == null) return false;
+		if (completeAssignment == null) 
+		{
+			_logger.Log($"Assignment not found by title: {title} \n Can't mark as complete.");
+			return false;
+		}
+		_logger.Log($"Assignment Completed: {_formatter.Format(completeAssignment)}");
 
 		completeAssignment.MarkComplete();
 		return true;
@@ -46,11 +61,16 @@ public class AssignmentService : IAssignmentService
 	public bool UpdateAssigment(string oldTitle, string newTitle, string newDescription)
 	{
 		Assignment? updateAssignment = FindAssignmentByTitle(oldTitle);
-		if (updateAssignment == null) return false;
+		if (updateAssignment == null) 
+		{
+			_logger.Log($"Assignment not found by title: {oldTitle} \n Can't Update.");
+			return false;
+		}
 		if (!oldTitle.Equals(newTitle, StringComparison.OrdinalIgnoreCase)
 			&& Assignments.Any(assi => assi.Title.Equals(newTitle, StringComparison.OrdinalIgnoreCase))
 			) return false;
 		
+		_logger.Log($"Assignment Updated: {_formatter.Format(updateAssignment)}\n With New Details: {newTitle}, {newDescription}");
 		updateAssignment.Update(newTitle, newDescription);
 		return true;
 	}
@@ -59,8 +79,12 @@ public class AssignmentService : IAssignmentService
 	{
 		Assignment? deleteAssignment = FindAssignmentByTitle(title);
 
-		if (deleteAssignment == null) return false;
-
+		if (deleteAssignment == null) 
+		{
+			_logger.Log($"Assignment not found by title: {title} \n Can't Delete.");
+			return false;
+		}
+		_logger.Log($"Deleting Assignment: {_formatter.Format(deleteAssignment)}");
 		Assignments.Remove(deleteAssignment);
 		return true;
 	}
